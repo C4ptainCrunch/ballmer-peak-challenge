c3936426e33dc480b112c9a8bfc2825f7609d1f4
Nikita Marchant <nikita.marchant@gmail.com>
2016-06-28 20:26:37 +0200

    ops

---
+++ ops.py
@@ -0,0 +1,25 @@
+import requests
+
+BASE = "http://172.23.218.41:5000/video_poker/api/"
+TOKEN = ""
+
+def info():
+    r = requests.get(BASE + "team/" + TOKEN)
+    return r.json()
+
+def refill():
+    r = requests.get(BASE + "refill/" + TOKEN)
+    return r.json()
+
+
+def step1(bet):
+    if info()['step'] != 0:
+        raise RuntimeError("Not good step")
+    r = requests.post(BASE + "game/" + TOKEN, data={'bet':bet})
+    return r.json()
+
+def step2(exchange):
+    if info()['step'] != 1:
+        raise RuntimeError("Not good step")
+    ex = "".join(map(lambda x: str(x), exchange))
+    r = requests.post(BASE + "game/" + TOKEN, data={'exchange':ex})
